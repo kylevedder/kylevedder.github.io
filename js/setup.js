5b1b1c5e12c9b9d844be807bf672c1e0c379278d
kylevedder <kylecvedder@gmail.com>
2014-12-17 11:33:01 -0500

    add css

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,19 @@
 
 $(function() {	
-$( "#accordion" ).accordion({
+$( "#accordion1" ).accordion({
   heightStyle: "content",
   collapsible: true,
   active: false
 });
+$( "#accordion2" ).accordion({
+  heightStyle: "content",
+  collapsible: true,
+  active: false
+});
+$( "#accordion3" ).accordion({
+  heightStyle: "content",
+  collapsible: true,
+  active: false
+});
+
 });
