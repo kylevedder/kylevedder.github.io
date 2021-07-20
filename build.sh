36033a5ab9f582b017bf70d577991cea5c76f9d4
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-07-20 10:41:17 -0400

    Added X* page and added it to build.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pandoc publications.md -o publications.html
 pandoc index.md -o index.html
+pandoc xstar.md -o xstar.html
 pandoc misc/mujoco_py.md -o misc/mujoco_py.html
 pandoc misc/screen.md -o misc/screen.html
