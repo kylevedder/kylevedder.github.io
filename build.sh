c8a7b8c9e8cb87cf9c91620febd7f2c2c0ecdf1f
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-31 17:12:12 -0400

    Added hash to metadata and hash check to build script so identical resumes aren't rebuilt each time, causing PDF churn.

--- build.sh
+++ build.sh
@@ -5,4 +5,11 @@ pandoc xstar.md -o xstar.html
 pandoc sparse_point_pillars.md -o sparse_point_pillars.html
 pandoc misc/mujoco_py.md -o misc/mujoco_py.html
 pandoc misc/screen.md -o misc/screen.html
-pandoc resume.md -o KyleVedderResume.pdf
+
+resume_hash=$(md5sum resume.md | awk '{print $1}');
+existing_resume_hash=$(exiftool KyleVedderResume.pdf | awk '/^Subject/' | awk '{print $3}');
+if [ "$resume_hash" != "$existing_resume_hash" ]; then
+  echo "Rebuilding resume";
+  pandoc resume.md -o KyleVedderResume.pdf
+  exiftool -overwrite_original -Subject="$resume_hash" KyleVedderResume.pdf
+fi
