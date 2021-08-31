db927e208279ba7bc3c7250f453799dc70c4cf7e
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-31 16:48:57 -0400

    Added sparse point pillars page.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 pandoc publications.md -o publications.html
 pandoc index.md -o index.html
 pandoc xstar.md -o xstar.html
+pandoc sparse_point_pillars.md -o sparse_point_pillars.html
 pandoc misc/mujoco_py.md -o misc/mujoco_py.html
 pandoc misc/screen.md -o misc/screen.html
 pandoc resume.md -o KyleVedderResume.pdf
