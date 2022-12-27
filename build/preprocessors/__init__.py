f57eb5ade9a5848a8fc3ed6ef0a117221cec1583
Kyle Vedder <kyle.c.vedder@gmail.com>
2022-12-27 00:10:15 -0500

    Added single statement of purpose to be included in resume and homepage

--- build/preprocessors/__init__.py
+++ build/preprocessors/__init__.py
@@ -2,8 +2,11 @@ from .header import process_header_line
 from .header import SENTINAL as HEADER_SENTINAL
 from .pubs import process_pub_line
 from .pubs import SENTINAL as PUBS_SENTINAL
+from .sop import process_sop_line
+from .sop import SENTINAL as SOP_SENTINAL
 
 processor_map = {
     HEADER_SENTINAL: process_header_line,
     PUBS_SENTINAL: process_pub_line,
+    SOP_SENTINAL: process_sop_line,
 }
