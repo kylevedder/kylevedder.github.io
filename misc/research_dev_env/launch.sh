874c24d22dee8cadee070aee29fa2f1f228ebace
Kyle Vedder <kyle.c.vedder@gmail.com>
2023-07-03 18:54:05 -0400

    Added blogpost about env

--- misc/research_dev_env/launch.sh
+++ misc/research_dev_env/launch.sh
@@ -2,10 +2,9 @@
 xhost +
 touch `pwd`/docker_history.txt
 docker run --gpus=all --rm -it \
- --shm-size=16gb \
  -v `pwd`:/project \
- -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v `pwd`/docker_history.txt:/root/.bash_history \
+ -v /tmp/.X11-unix:/tmp/.X11-unix \
  -e DISPLAY=$DISPLAY \
  -h $HOSTNAME \
  --privileged \
