72a491eddbd3444343cdea9adcdbb4d16d63c193
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-09-09 19:59:36 -0400

    Added gigachad jack demo. It's got a lot of flow

---
+++ gigachad.md
@@ -0,0 +1,109 @@
+HEADER {"page_name": "GIGACHAD", "teaser_img": "http://vedder.io/img/static/trackflow/teaser.png"}
+
+<style>
+#jack-flow-render-container {
+    width: 100%;
+    height: 600px;
+    border: 1px solid #ccc;
+    margin-top: 20px;
+}
+#jack-flow-slider-container {
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+}
+#jack-flow-frame-slider {
+    flex-grow: 1;
+    margin-right: 10px;
+}
+#jack-flow-frame-number {
+    width: 50px;
+    text-align: right;
+}
+
+#jack-traj-render-container {
+    width: 100%;
+    height: 600px;
+    border: 1px solid #ccc;
+    margin-top: 20px;
+}
+#jack-traj-slider-container {
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+}
+#jack-traj-frame-slider {
+    flex-grow: 1;
+    margin-right: 10px;
+}
+#jack-traj-frame-number {
+    width: 50px;
+    text-align: right;
+}
+
+
+</style>
+
+
+# GIGACHAD
+
+## [Kyle Vedder](http://vedder.io), [Neehar Peri](http://www.neeharperi.com/), [Ishan Khatri](https://ishan.khatri.io/),  [Eric Eaton](https://www.seas.upenn.edu/~eeaton/), [Yue Wang](https://yuewang.xyz/), [Zhiding Yu](https://chrisding.github.io/), and [Joachim Pehserl](https://www.linkedin.com/in/joachim-pehserl-45514a98/)
+
+
+<div class="centered">
+# [[Paper]](TODO)
+</div>
+
+
+<!-- Side by side images from img/static/gigachad/gigachad_bird_flow_cropped.png and  img/static/gigachad/gigachad_bird_trajectory_cropped.png -->
+<div style="display: flex" class="centered">
+<img src="img/static/gigachad/gigachad_bird_flow_cropped.png" style="width:49%;" />
+<img src="img/static/gigachad/gigachad_bird_trajectory_cropped.png" style="width:49%"/>
+</div>
+
+<script type="importmap">
+{
+    "imports": {
+        "three": "https://unpkg.com/three@0.159.0/build/three.module.js",
+        "three/addons/": "https://unpkg.com/three@0.159.0/examples/jsm/"
+    }
+}
+</script>
+
+## Table Top Scene Flow
+
+<div class="centered">
+<div id="jack-flow-render-container"></div>
+<div id="jack-flow-slider-container">
+<input type="range" id="jack-flow-frame-slider" min="0" max="16" value="0" class="centered">
+Frame <span id="jack-flow-frame-number">0</span>
+</div>
+Use the slider or arrow keys to navigate through the frames.
+</div>
+<script type="module" src="js/jack_scene_flow_vis.js"></script>
+
+
+## Table Top Object Tracking
+
+<div class="centered">
+<div id="jack-traj-render-container"></div>
+<div id="jack-traj-slider-container">
+<input type="range" id="jack-traj-frame-slider" min="0" max="16" value="0" class="centered">
+Frame <span id="jack-traj-frame-number">0</span>
+</div>
+Use the slider or arrow keys to navigate through the frames.
+</div>
+<script type="module" src="js/jack_traj_vis.js"></script>
+
+
+## Videos
+
+
+
+## Citation
+
+```
+TODO
+```
---
+++ js/jack_scene_flow_vis.js
@@ -0,0 +1,153 @@
+import * as THREE from 'three';
+import { PLYLoader } from 'three/addons/loaders/PLYLoader.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+
+const scene = new THREE.Scene();
+const container = document.getElementById('jack-flow-render-container');
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
+
+const controls = new TrackballControls(camera, renderer.domElement);
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 0, 10);
+scene.add(light);
+
+let currentPoints = null;
+let flowLines = null;
+let isFirstLoad = true;
+
+const slider = document.getElementById('jack-flow-frame-slider');
+const frameNumber = document.getElementById('jack-flow-frame-number');
+
+function padNumber(number) {
+    return number.toString().padStart(4, '0');
+}
+
+function loadPLYAndFlow(frameIndex) {
+    const plyFileName = `img/static/gigachad/raw_data/jack_spinning/${padNumber(frameIndex)}.ply`;
+    const jsonFileName = `img/static/gigachad/raw_data/jack_spinning/${padNumber(frameIndex)}.json`;
+    const plyLoader = new PLYLoader();
+    
+    Promise.all([
+        new Promise((resolve, reject) => {
+            plyLoader.load(plyFileName, resolve, undefined, reject);
+        }),
+        fetch(jsonFileName).then(response => response.json())
+    ]).then(([geometry, flowData]) => {
+        if (currentPoints) {
+            scene.remove(currentPoints);
+        }
+        if (flowLines) {
+            scene.remove(flowLines);
+        }
+
+        const material = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
+        const points = new THREE.Points(geometry, material);
+        
+        scene.add(points);
+        currentPoints = points;
+
+        // Create flow lines
+        const lineGeometry = new THREE.BufferGeometry();
+        const positions = [];
+        const colors = [];
+
+        const pointPositions = geometry.attributes.position.array;
+        for (let i = 0; i < pointPositions.length; i += 3) {
+            const startX = pointPositions[i];
+            const startY = pointPositions[i + 1];
+            const startZ = pointPositions[i + 2];
+            
+            const flow = flowData[i / 3];
+            const endX = startX + flow[0];
+            const endY = startY + flow[1];
+            const endZ = startZ + flow[2];
+
+            positions.push(startX, startY, startZ);
+            positions.push(endX, endY, endZ);
+
+            // Color gradient from blue to red
+            colors.push(0, 0, 1);
+            colors.push(1, 0, 0);
+        }
+
+        lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+        lineGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+        const lineMaterial = new THREE.LineBasicMaterial({ vertexColors: true });
+        flowLines = new THREE.LineSegments(lineGeometry, lineMaterial);
+        scene.add(flowLines);
+
+        if (isFirstLoad) {
+            // Set up the camera only on first load
+            const box = new THREE.Box3().setFromObject(points);
+            const size = box.getSize(new THREE.Vector3());
+            const maxDim = Math.max(size.x, size.y, size.z);
+            const distance = maxDim * 0.4;
+            const angle = Math.PI / 6;
+            camera.position.set(
+                -distance * Math.cos(angle),
+                -distance * Math.sin(angle),
+                distance * 0.5
+            );
+            camera.up.set(0, 0, 1);
+            camera.lookAt(0, 0, 0);
+            controls.target.set(0, 0, 0);
+            controls.update();
+
+            isFirstLoad = false;
+        }
+
+    }).catch(error => {
+        console.error('An error occurred while loading the PLY file or flow data:', error);
+    });
+}
+
+function updateFrame(frameIndex) {
+    frameIndex = Math.max(0, Math.min(16, frameIndex));
+    slider.value = frameIndex;
+    frameNumber.textContent = frameIndex;
+    loadPLYAndFlow(frameIndex);
+}
+
+slider.addEventListener('input', function() {
+    updateFrame(parseInt(this.value));
+});
+
+document.addEventListener('keydown', function(event) {
+    const currentFrame = parseInt(slider.value);
+    if (event.key === 'ArrowLeft') {
+        updateFrame(currentFrame - 1);
+    } else if (event.key === 'ArrowRight') {
+        updateFrame(currentFrame + 1);
+    }
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', function() {
+    const newWidth = container.clientWidth;
+    const newHeight = container.clientHeight;
+    camera.aspect = newWidth / newHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(newWidth, newHeight);
+    controls.handleResize();
+});
+
+// Initial load
+loadPLYAndFlow(0);
